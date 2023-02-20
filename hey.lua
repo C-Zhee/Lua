8c8122f7ea487381aeec0310aea7086e2f94c58e
Corey <coreyzhee@gmail.com>
2023-02-20 00:00:50 -0500

    Lua

---
+++ hey.lua
@@ -0,0 +1,19 @@
+print ("Hello, World!")
+-- block out text
+
+print("💜💨")
+-- this will actually print out both emojis!
+
+print("Hello")
+print("Goodbye")
+-- Lua prints from top to bottom, so we will see: Hello
+                                                --Goodbye  
+
+
+hello = "Hi Mom"
+print(hello)
+
+--[[To comment out multiple lines,
+just do something along the 
+lines of this!
+]]
